9c84b32fac0285673b247f483ba32d3c4fc75a8b
Kirk Mayo <kirk@stripetheweb.com>
2016-05-24 16:01:19 +1200

    Gutted phockito like a Butcher

--- _config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if(class_exists('SS_ClassManifest')) {
-	require_once(dirname(__FILE__).'/code/PhockitoClassManifestUpdater.php');
-}
-else {
-	require_once(dirname(__FILE__).'/code/PhockitoClassGlobalUpdater.php');
-}
